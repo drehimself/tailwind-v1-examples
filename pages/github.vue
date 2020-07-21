aa86d57ba3c4f107c231b0b283685aa7028e6e38
Andre Madarang <andre_madarang@hotmail.com>
2020-07-21 01:22:36 -0400

    GitHub - Top Navs complete

---
+++ pages/github.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="text-gray-900 text-sm">
+    <nav class="bg-gray-900 text-white px-4 py-3 flex items-center justify-between">
+      <div class="flex items-center space-x-4">
+        <a href="#" class="text-white hover:text-gray-400">
+          <svg class="w-8 fill-current" viewBox="0 0 16 16" aria-hidden="true"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg>
+        </a>
+        <div class="relative">
+          <input type="text" class="rounded bg-gray-700 placeholder-white w-72 px-3 py-1" placeholder="Search or jump to...">
+          <div class="absolute top-0 right-0 flex items-center h-full">
+            <div class="border border-gray-600 rounded text-xs text-gray-400 px-2 mr-2">/</div>
+          </div>
+        </div>
+        <ul class="flex items-center font-semibold space-x-4">
+          <li><a href="#" class="hover:text-gray-400">Pull Requests</a></li>
+          <li><a href="#" class="hover:text-gray-400">Issues</a></li>
+          <li><a href="#" class="hover:text-gray-400">Marketplace</a></li>
+          <li><a href="#" class="hover:text-gray-400">Explore</a></li>
+        </ul>
+      </div>
+      <div class="flex items-center space-x-4 text-white">
+        <a href="#" class="relative hover:text-gray-400">
+          <svg class="w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path></svg>
+          <div class="bg-blue-600 rounded-full w-2 h-2 absolute top-0 right-0"></div>
+        </a>
+        <a href="#" class="flex items-center hover:text-gray-400">
+          <svg class="w-4" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M12 4v16m8-8H4"></path></svg>
+          <svg class="w-3 ml-1" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M19 9l-7 7-7-7"></path></svg>
+        </a>
+        <a href="#" class="flex items-center hover:text-gray-400">
+          <img src="https://avatars0.githubusercontent.com/u/4316355?s=60&v=4" class="w-5 h-5 rounded-full" alt="avatar">
+          <svg class="w-3 ml-1" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M19 9l-7 7-7-7"></path></svg>
+        </a>
+      </div>
+    </nav>
+    <div class="repo-stats flex items-center justify-between px-8 py-4">
+      <div class="flex">
+        <svg class="w-4 fill-current text-gray-600" viewBox="0 0 16 16" aria-hidden="true"><path fill-rule="evenodd" d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z"></path></svg>
+        <div class="flex items-center text-xl ml-2">
+          <a href="#" class="text-blue-600 hover:underline">tailwindcss</a>
+          <span class="ml-1">/</span>
+          <a href="#" class="text-blue-600 hover:underline font-semibold ml-1">tailwindcss</a>
+        </div>
+      </div>
+      <div class="flex space-x-2">
+        <div class="flex text-xs">
+          <button class="border border-gray-400 rounded rounded-r-none px-3 py-1 flex items-center space-x-1">
+            <svg class="fill-current text-gray-600 w-4" viewBox="0 0 16 16" aria-hidden="true"><path fill-rule="evenodd" d="M1.679 7.932c.412-.621 1.242-1.75 2.366-2.717C5.175 4.242 6.527 3.5 8 3.5c1.473 0 2.824.742 3.955 1.715 1.124.967 1.954 2.096 2.366 2.717a.119.119 0 010 .136c-.412.621-1.242 1.75-2.366 2.717C10.825 11.758 9.473 12.5 8 12.5c-1.473 0-2.824-.742-3.955-1.715C2.92 9.818 2.09 8.69 1.679 8.068a.119.119 0 010-.136zM8 2c-1.981 0-3.67.992-4.933 2.078C1.797 5.169.88 6.423.43 7.1a1.619 1.619 0 000 1.798c.45.678 1.367 1.932 2.637 3.024C4.329 13.008 6.019 14 8 14c1.981 0 3.67-.992 4.933-2.078 1.27-1.091 2.187-2.345 2.637-3.023a1.619 1.619 0 000-1.798c-.45-.678-1.367-1.932-2.637-3.023C11.671 2.992 9.981 2 8 2zm0 8a2 2 0 100-4 2 2 0 000 4z"></path></svg>
+            <div>Watch</div>
+            <svg class="w-3 ml-1" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M19 9l-7 7-7-7"></path></svg>
+          </button>
+          <a href="#" class="border border-gray-400 rounded rounded-l-none border-l-0 font-semibold hover:text-blue-600 px-3 py-1">430</a>
+        </div>
+        <div class="flex text-xs">
+          <button class="border border-gray-400 rounded rounded-r-none px-3 py-1 hover:bg-gray-200 flex items-center space-x-1">
+            <svg class="fill-current text-gray-600 w-4" viewBox="0 0 16 16" version="1.1" width="16" aria-hidden="true"><path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"></path></svg>
+            <div class="">Star</div>
+          </button>
+          <a href="#" class="border border-gray-400 border-l-0 rounded rounded-l-none font-semibold px-3 py-1 hover:text-blue-600">25.3k</a>
+        </div>
+        <div class="flex text-xs">
+          <button class="border border-gray-400 rounded rounded-r-none px-3 py-1 hover:bg-gray-200 flex items-center space-x-1">
+            <svg class="fill-current text-gray-600 w-4" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"></path></svg>
+            <div class="">Fork</div>
+          </button>
+          <a href="#" class="border border-gray-400 border-l-0 rounded rounded-l-none font-semibold px-3 py-1 hover:text-blue-600">1.2k</a>
+        </div>
+      </div>
+    </div> <!-- end repo-stats -->
+
+    <ul class="repo-nav border-b border-gray-400 flex items-center px-8 mt-4">
+      <li class="font-semibold">
+        <a href="#" class="flex items-center border-b-2 border-red-500 px-4 pb-3">
+          <svg class="w-4 fill-current" classes="UnderlineNav-octicon" display="none inline" viewBox="0 0 16 16" version="1.1" width="16" aria-hidden="true"><path fill-rule="evenodd" d="M4.72 3.22a.75.75 0 011.06 1.06L2.06 8l3.72 3.72a.75.75 0 11-1.06 1.06L.47 8.53a.75.75 0 010-1.06l4.25-4.25zm6.56 0a.75.75 0 10-1.06 1.06L13.94 8l-3.72 3.72a.75.75 0 101.06 1.06l4.25-4.25a.75.75 0 000-1.06l-4.25-4.25z"></path></svg>
+          <span class="ml-2">Code</span>
+        </a>
+        <li>
+          <a href="#" class="flex items-center border-b-2 border-transparent hover:border-gray-400 transition ease-in-out duration-150 px-4 pb-3">
+            <svg class="w-4 fill-current text-gray-600" classes="UnderlineNav-octicon" display="none inline" viewBox="0 0 16 16" aria-hidden="true"><path fill-rule="evenodd" d="M8 1.5a6.5 6.5 0 100 13 6.5 6.5 0 000-13zM0 8a8 8 0 1116 0A8 8 0 010 8zm9 3a1 1 0 11-2 0 1 1 0 012 0zm-.25-6.25a.75.75 0 00-1.5 0v3.5a.75.75 0 001.5 0v-3.5z"></path></svg>
+            <span class="ml-2">Issues</span>
+            <div class="text-xs rounded-lg bg-gray-300 px-2 ml-1">106</div>
+          </a>
+        </li>
+      <li>
+        <a href="#" class="flex items-center border-b-2 border-transparent px-4 pb-3 hover:border-gray-400 transition ease-in duration-150">
+          <svg class="w-4 fill-current text-gray-600" classes="UnderlineNav-octicon" display="none inline" viewBox="0 0 16 16" version="1.1" width="16" aria-hidden="true"><path fill-rule="evenodd" d="M7.177 3.073L9.573.677A.25.25 0 0110 .854v4.792a.25.25 0 01-.427.177L7.177 3.427a.25.25 0 010-.354zM3.75 2.5a.75.75 0 100 1.5.75.75 0 000-1.5zm-2.25.75a2.25 2.25 0 113 2.122v5.256a2.251 2.251 0 11-1.5 0V5.372A2.25 2.25 0 011.5 3.25zM11 2.5h-1V4h1a1 1 0 011 1v5.628a2.251 2.251 0 101.5 0V5A2.5 2.5 0 0011 2.5zm1 10.25a.75.75 0 111.5 0 .75.75 0 01-1.5 0zM3.75 12a.75.75 0 100 1.5.75.75 0 000-1.5z"></path></svg>
+          <span class="ml-2">Pull Requests</span>
+          <div class="text-xs rounded-lg bg-gray-300 px-2 ml-1">25</div>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="flex items-center border-b-2 border-transparent px-4 pb-3 hover:border-gray-400 transition ease-in duration-150">
+          <svg class="w-4 fill-current text-gray-600" classes="UnderlineNav-octicon" display="none inline" viewBox="0 0 16 16" version="1.1" width="16" aria-hidden="true"><path fill-rule="evenodd" d="M1.5 2.75a.25.25 0 01.25-.25h8.5a.25.25 0 01.25.25v5.5a.25.25 0 01-.25.25h-3.5a.75.75 0 00-.53.22L3.5 11.44V9.25a.75.75 0 00-.75-.75h-1a.25.25 0 01-.25-.25v-5.5zM1.75 1A1.75 1.75 0 000 2.75v5.5C0 9.216.784 10 1.75 10H2v1.543a1.457 1.457 0 002.487 1.03L7.061 10h3.189A1.75 1.75 0 0012 8.25v-5.5A1.75 1.75 0 0010.25 1h-8.5zM14.5 4.75a.25.25 0 00-.25-.25h-.5a.75.75 0 110-1.5h.5c.966 0 1.75.784 1.75 1.75v5.5A1.75 1.75 0 0114.25 12H14v1.543a1.457 1.457 0 01-2.487 1.03L9.22 12.28a.75.75 0 111.06-1.06l2.22 2.22v-2.19a.75.75 0 01.75-.75h1a.25.25 0 00.25-.25v-5.5z"></path></svg>
+          <span class="ml-2">Discussions</span>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="flex items-center border-b-2 border-transparent px-4 pb-3 hover:border-gray-400 transition ease-in duration-150">
+          <svg class="w-4 fill-current text-gray-600" classes="UnderlineNav-octicon" display="none inline" viewBox="0 0 16 16" version="1.1" width="16" aria-hidden="true"><path fill-rule="evenodd" d="M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zM6.379 5.227A.25.25 0 006 5.442v5.117a.25.25 0 00.379.214l4.264-2.559a.25.25 0 000-.428L6.379 5.227z"></path></svg>
+          <span class="ml-2">Actions</span>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="flex items-center border-b-2 border-transparent px-4 pb-3 hover:border-gray-400 transition ease-in duration-150">
+          <svg class="w-4 fill-current text-gray-600" classes="UnderlineNav-octicon" display="none inline" viewBox="0 0 16 16" version="1.1" width="16" aria-hidden="true"><path fill-rule="evenodd" d="M7.467.133a1.75 1.75 0 011.066 0l5.25 1.68A1.75 1.75 0 0115 3.48V7c0 1.566-.32 3.182-1.303 4.682-.983 1.498-2.585 2.813-5.032 3.855a1.7 1.7 0 01-1.33 0c-2.447-1.042-4.049-2.357-5.032-3.855C1.32 10.182 1 8.566 1 7V3.48a1.75 1.75 0 011.217-1.667l5.25-1.68zm.61 1.429a.25.25 0 00-.153 0l-5.25 1.68a.25.25 0 00-.174.238V7c0 1.358.275 2.666 1.057 3.86.784 1.194 2.121 2.34 4.366 3.297a.2.2 0 00.154 0c2.245-.956 3.582-2.104 4.366-3.298C13.225 9.666 13.5 8.36 13.5 7V3.48a.25.25 0 00-.174-.237l-5.25-1.68zM9 10.5a1 1 0 11-2 0 1 1 0 012 0zm-.25-5.75a.75.75 0 10-1.5 0v3a.75.75 0 001.5 0v-3z"></path></svg>
+          <span class="ml-2">Security</span>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="flex items-center border-b-2 border-transparent px-4 pb-3 hover:border-gray-400 transition ease-in duration-150">
+          <svg class="w-4 fill-current text-gray-600" classes="UnderlineNav-octicon" display="none inline" viewBox="0 0 16 16" version="1.1" width="16" aria-hidden="true"><path fill-rule="evenodd" d="M1.5 1.75a.75.75 0 00-1.5 0v12.5c0 .414.336.75.75.75h14.5a.75.75 0 000-1.5H1.5V1.75zm14.28 2.53a.75.75 0 00-1.06-1.06L10 7.94 7.53 5.47a.75.75 0 00-1.06 0L3.22 8.72a.75.75 0 001.06 1.06L7 7.06l2.47 2.47a.75.75 0 001.06 0l5.25-5.25z"></path></svg>
+          <span class="ml-2">Insights</span>
+        </a>
+      </li>
+      </li>
+    </ul>
+  </div>
+</template>
